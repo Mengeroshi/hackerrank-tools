90a535cf80875898e3a4135fb3c4fecacbc626c9
mengeroshi <dpllgr@hotmail.com>
2020-01-14 22:17:57 -0600

    print alway with 2 decimals

---
+++ idk.py
@@ -0,0 +1,15 @@
+obo = {'Krishna': [67.0, 68.0, 69.0], 'Arjun': [70.0, 98.0, 63.0], 'Malika': [52.0, 56.0, 60.0]}
+
+
+
+print(obo['Arjun'])
+
+print(len(obo['Arjun']))
+
+suma = sum(obo['Arjun'])
+largo = len(obo['Arjun'])
+average = suma/largo
+print(f"{average:.2f}")
+
+print(f"{average:.2f}")
+print(f"{1.1234:.2f}")
